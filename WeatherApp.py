2cc56517fb8c9cd45b6f9274119ee6ab0fc2db0a
NesanduD <ndissaka@gmail.com>
2024-05-24 18:17:32 +0530

    finished getting data till msn.

--- WeatherApp.py
+++ WeatherApp.py
@@ -13,12 +13,25 @@ header = Headers(
 def responses(url):  
     return requests.get(url, headers=header.generate())
 
+def status_check(resp):
+    if resp.status_code != 200:
+        return True
+    else:
+        return False
+    
+# arrays
+today_rain_day      = [] # array for today daytime rain probablity
+today_rain_night    = [] # array for tonight rain prob.
+tomorrow_rain_day   = [] # array for tomorrow daytime rain probablity
+tomorrow_rain_night = [] # array for tomorrow night rain prob.
+
+
 # URLS
 accuweather_url    = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/309472?apikey=IeS7q8uvP3iefn51Qa4ReCfNihJgrGqU&details=true&metric=true'
-weathercom_url     = 'https://weather.com/weather/tenday/l/746ec06059a777b4fe27c23654cd84da9bf2c4f2ebc7ee53f098aaf52d94eb87'
-weatheratlas_url   = 'https://www.weather-atlas.com/en/sri-lanka/kuruwita-long-term-weather-forecast'
+weathercom_url     = 'https://weather.com/weather/tenday/l/fb23578d1e77fc9465448c2aa52f6e72e9b5f380bada48f4e626070fe3920cb2'
+weatheratlas_url   = 'https://www.weather-atlas.com/en/sri-lanka/kuruwita-weather-tomorrow'
 timeanddatecom_url = 'https://www.timeanddate.com/weather/@1237940/ext'
-msn_url            = 'https://www.msn.com/en-us/weather/forecast/in-Kuruwita,Sabaragamuwa-Province?loc=eyJsIjoiS3VydXdpdGEiLCJyIjoiU2FiYXJhZ2FtdXdhIFByb3ZpbmNlIiwicjIiOiJSYXRuYXB1cmEgRGlzdHJpY3QiLCJjIjoiU3JpIExhbmthIiwiaSI6IkxLIiwidCI6MTAyLCJnIjoiZW4tdXMiLCJ4IjoiODAuMzY2OSIsInkiOiI2Ljc3NjEifQ%3D%3D&weadegreetype=C&cvid=4223c3f4c8dd462f9acbe0807eed5833'
+msn_url            = 'https://www.msn.com/en-us/weather/hourlyforecast/in-Kuruwita,Sabaragamuwa-Province?loc=eyJsIjoiS3VydXdpdGEiLCJyIjoiU2FiYXJhZ2FtdXdhIFByb3ZpbmNlIiwicjIiOiJSYXRuYXB1cmEgRGlzdHJpY3QiLCJjIjoiU3JpIExhbmthIiwiaSI6IkxLIiwidCI6MTAyLCJnIjoiZW4tc2ciLCJ4IjoiODAuMzY2ODk3NTgzMDA3ODEiLCJ5IjoiNi43NzYxMDAxNTg2OTE0MDYifQ%3D%3D&weadegreetype=C&cvid=651008f8734e43af9ab5ac9e1e3ba583'
 
 
 # Responses
@@ -35,22 +48,15 @@ if msn_response.status_code != 200:
 else:
     print("connection established with msn.com. ")
     msn_html = requests.get(msn_url).text
-    
     # msn.com soupobject
     msn_soup = BeautifulSoup(msn_html,'html.parser')
-    precip_elements = msn_soup.find_all('div', {'title': 'Precipitation'})
-    print(precip_elements)
-
+    precip_elements = msn_soup.find_all('div', {'title': 'Chance of precipitation'})
     for precip_element in precip_elements:
         # Find all span elements inside the current precipitation div
         span_elements = precip_element.find_all('span')
-        today_precipitation_percentage = span_elements[1].text.strip()
-        print(today_precipitation_percentage)
+        msn_today = span_elements[1].text.strip()
+        print(span_elements)
         # still only today weather scraped. need to scrape msn tomorrow too
-
-
-            
-    
 '''
 # Timeanddatecom code
 if timeaddatecom_response.status_code != 200:
@@ -70,6 +76,15 @@ else:
         # Check if the text is a percentage value
         if '%' in text:
             timeanddatecom_percentage_values.append(text)
+
+    timeanddatecom_percentage_values=[int(p.strip("%")) for p in timeanddatecom_percentage_values ]
+    today_rain_day.append(timeanddatecom_percentage_values[0])
+    today_rain_night.append(timeanddatecom_percentage_values[0])
+
+    tomorrow_rain_day.append(timeanddatecom_percentage_values[1])
+    tomorrow_rain_day.append(timeanddatecom_percentage_values[1])
+
+
 
 # Weather-atlas code
 if weather_atlas_response.status_code !=200:
@@ -94,7 +109,15 @@ else:
                 precip_probability = li.find('span', {'class': 'fw-bold'}).get_text(strip=True)
                 weather_atlas_percentage_values.append(precip_probability)
                 # Break out of the loop since we found the desired value
-                break # testing purpose
+    # some processigng
+    weather_atlas_percentage_values_day   = weather_atlas_percentage_values[4:11]
+    weather_atlas_percentage_values_day   = [int(p.strip("%")) for p in weather_atlas_percentage_values_day]
+    weather_atlas_percentage_values_night = weather_atlas_percentage_values[11:]
+    weather_atlas_percentage_values_night = [int(p.strip("%")) for p in weather_atlas_percentage_values_night]
+    # addng to main array (tomorrow)
+    tomorrow_rain_day.append(weather_atlas_percentage_values_day/len(weather_atlas_percentage_values_day))
+    tomorrow_rain_night.append(weather_atlas_percentage_values_night/len(weather_atlas_percentage_values_night)) 
+
 
 # Weathercom code
 if weathercom_response.status_code != 200:
@@ -114,9 +137,13 @@ else:
     for element in percentage_value_elements:
         percentage_value = element.text
         weathercom_percentage_values.append(percentage_value)
-        
+    
+        today_rain_day.append(weathercom_percentage_values[0]) # adding weathercom day time rain prob. 
+        today_rain_night.append(weathercom_percentage_values[4]) # adding tonight rain
+
+
 # Accuweather code
-if accuweather_response.status_code != 200:
+if status_check(accuweather_response):
     print("Connection failed with accuweather. Error code: ",accuweather_response.status_code)
 else:
     print("connection established with accuweather.")
@@ -126,89 +153,28 @@ else:
     
     # get preciptitation data from all daily forecasts of accuweather
     accuweather_daily_forecasts = data.get('DailyForecasts', [])   
-    accuweather_precipitation_probability = [adf.get('Day', {}).get('PrecipitationProbability', None) for adf in accuweather_daily_forecasts]
-    
+    accuweather_precipitation_probability_day = [adf.get('Day', {}).get('PrecipitationProbability', None) for adf in accuweather_daily_forecasts]
+    accuweather_precipitation_probability_night = [b.get('Night', {}).get('PrecipitationProbability', None) for b in accuweather_daily_forecasts]
+   
     # Preciptitation information
-    today_precipitation_accuweather = accuweather_precipitation_probability[0]
-    tomorrow_precipitation_accuweather = accuweather_precipitation_probability[1]
+    today_rain_day.append(accuweather_precipitation_probability_day[0])
+    today_rain_night.append(accuweather_precipitation_probability_night[0])
 
 
-    '''   
+ ''' 
 
 ''' weathercom output
-Percentage Value: 96% today rain
-Percentage Value: 96% today rain
-Percentage Value: 86% humid today
-Percentage Value: 59% rain tonight
-Percentage Value: 98% tomorrow day rain
-Percentage Value: 97% humid
-Percentage Value: 97% rain tomorrow day
-Percentage Value: 84% humid
-Percentage Value: 87% rain tomorrow night
-Percentage Value: 92%
-Percentage Value: 83% humid
-Percentage Value: 68% night rain 11
-Percentage Value: 98% humid
-Percentage Value: 99%
-Percentage Value: 99%
-Percentage Value: 83%
-Percentage Value: 78%
-Percentage Value: 99%
-    Percentage Value: 95%
-Percentage Value: 95%
-    Percentage Value: 85%
-Percentage Value: 60%
-Percentage Value: 99%
-Percentage Value: 85%
-Percentage Value: 85%
-Percentage Value: 85%
-Percentage Value: 72%
-Percentage Value: 99%
-    Percentage Value: 76%
-Percentage Value: 76%
-Percentage Value: 86%
-Percentage Value: 73%
-Percentage Value: 99%
-    Percentage Value: 66%
-Percentage Value: 66%
-Percentage Value: 86%
-Percentage Value: 24%
-Percentage Value: 98%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 85%
-Percentage Value: 60%
-Percentage Value: 99%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 89%
-Percentage Value: 60%
-Percentage Value: 99%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 89%
-Percentage Value: 60%
-Percentage Value: 99%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 88%
-Percentage Value: 60%
-Percentage Value: 99%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 88%
-Percentage Value: 60%
-Percentage Value: 98%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 87%
-Percentage Value: 60%
-Percentage Value: 98%
-Percentage Value: 60%
-Percentage Value: 60%
-Percentage Value: 88%
-Percentage Value: 60%
-Percentage Value: 99%
+0 =  today rain
+1 =  today rain
+2 =  today humid
+3 =  tonight rain 
+4 =  tonight humid 
+5 =  tomorrow day rain
+6 =  tomorrow day rain
+7 =  tomorrow day humid
+8 =  tomorrow night rain
+9 = tomorrow night humid
+
 '''
 
 '''weather_atlas output
@@ -225,7 +191,5 @@ Precip. probability: 67%
 Precip. probability: 60%
 Precip. probability: 60%'''
 
-'''timeanddatecom output
-easy. firstone today second tomorrow'''
 
 
